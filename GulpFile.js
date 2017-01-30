5e8954c188d51bc1913ee0b9cdef16f536b3f505
Tommy Saputra <tommysaputra@Tommys-MacBook-Air.local>
2017-01-30 22:27:38 +1100

    this is template

---
+++ GulpFile.js
@@ -0,0 +1,40 @@
+var
+  gulp           = require('gulp'),
+  sass           = require('gulp-sass')
+// WATCH
+gulp.task('watch', watch);
+
+// Compile sass files and move them to dist/styles
+gulp.task('sass', compileSass);
+
+
+gulp.task('build', ['sass']);
+
+gulp.task('default', ['watch']);
+
+function watch() {
+  gulp.watch('/scss/**/*.scss', ['sass']);
+}
+
+
+function compileSass() {
+  return gulp.src('/scss/main.scss')
+             .pipe(sass())
+             .pipe(gulp.dest('dist/styles'));
+}
+
+
+var gulp        = require('gulp');
+var browserSync = require('browser-sync').create();
+
+// Static server
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./dist/"
+        }
+    });
+    gulp.watch('sass/**/*.scss', ['sass']);
+    gulp.watch('dist/**/*.html').on('change', browserSync.reload);
+});
+
